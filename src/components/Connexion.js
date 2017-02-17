2b3d999ec07773b3410a8888a8aa9af1f13fe3eb
christophe <chrismuzikos>
2017-02-17 22:46:17 +0100

    create component App, Connexion and NotFound. Start project

---
+++ src/components/Connexion.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class Connexion extends React.Component {
+
+    goToApp = event => {
+        event.preventDefault();
+        // on recupere le pseudo
+        const pseudo = this.boxInput.value;
+        // on change d'url
+        this.context.router.transitionTo(`/box/${pseudo}`);
+    }
+
+    render() {
+        return (
+            <div className="connexionBox">
+                <form className="connexion" onSubmit={e => this.goToApp(e)} >
+                    <h1>Ma Boîte à Recettes</h1>
+                    <input
+                        type="text"
+                        placeholder="Nom du Chef"
+                        pattern="[A-za-z-]{1,}"
+                        required
+                        ref={input => {this.boxInput = input}}
+                     />
+                     <button type="submit">GO</button>
+                     <p>Pas de caractères spéciaux</p>
+                </form>
+            </div>
+        )
+    }
+
+    static contextTypes = {
+        router: React.PropTypes.object
+    };
+}
+
+export default Connexion;
